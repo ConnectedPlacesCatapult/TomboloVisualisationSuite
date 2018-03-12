58be2ac4d27c8622a472abc5d4cf29b138505987
Robin Summerhill <robin.summerhill@emu-analytics.com>
2018-03-12 18:14:27 +0000

    Make mapAssetsUrl configurable to allow serving of assets from specified host

--- src/shared/style-generator/style-generator.ts
+++ src/shared/style-generator/style-generator.ts
@@ -29,7 +29,9 @@ export class StyleGenerator {
     style.zoom = this.mapDefinition.zoom || style.zoom;
     style.center = this.mapDefinition.center || style.center;
     style.sources = {...style['sources'], ...this.generateSources(this.mapDefinition)};
-    style.sources = this.expandTileSources(this.mapDefinition.tileBaseUrl, style.sources);
+    style.sources = this.expandTileSources(this.mapDefinition.tileUrl, style.sources);
+    style.glyphs = this.expandRelativeURL(this.mapDefinition.mapAssetsUrl, style.glyphs);
+    style.sprite = this.expandRelativeURL(this.mapDefinition.mapAssetsUrl, style.sprite);
 
     // Find layer indices of insertion points
     let insertionPoints = style.metadata.insertionPoints || {};
@@ -274,17 +276,17 @@ export class StyleGenerator {
 
       // For vector source with tileJSON url
       if (source.hasOwnProperty('url')) {
-        source = {...source, url: this.expandRelativeTileURL(baseUrl, source['url'])};
+        source = {...source, url: this.expandRelativeURL(baseUrl, source['url'])};
       }
 
       // For vector sources with inline tiles url
       if (source.hasOwnProperty('tiles')) {
-        source = {...source, tiles: source['tiles'].map(tileUrl => this.expandRelativeTileURL(baseUrl, tileUrl))};
+        source = {...source, tiles: source['tiles'].map(tileUrl => this.expandRelativeURL(baseUrl, tileUrl))};
       }
 
       // For geojson sources
       if (source.hasOwnProperty('data')) {
-        source = {...source, data: this.expandRelativeTileURL(baseUrl, source['data'])};
+        source = {...source, data: this.expandRelativeURL(baseUrl, source['data'])};
       }
 
       accum[key] = source;
@@ -293,7 +295,7 @@ export class StyleGenerator {
     }, {});
   }
 
-  private expandRelativeTileURL(baseUrl, url: string): string {
+  private expandRelativeURL(baseUrl, url: string): string {
     return (url.startsWith('http')) ? url : baseUrl + url;
   }
 
